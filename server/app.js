b09436636ddca5545d6482d9acd98ef4e88a1ee6
rubynikal <nika.zaharova06@gmail.com>
2025-10-30 18:54:57 +0000

    30.10

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import { Hono } from '@hono/hono';
 import { cors } from '@hono/hono/cors';
 import { logger } from '@hono/hono/logger';
+import postgres from "postgres";
 
 const app = new Hono();
+const sql = postgres();
 
 //cors un logger added to all (*)
 app.use("/*", cors()); 
@@ -10,5 +12,9 @@ app.use("/*", logger());
 
 //define path
 app.get("/", (c) => c.json({message: "Hello world!" }));
+app.get("/todos", async (c) => {
+    const todos = await sql`SELECT * FROM todos`;
+    return c.json(todos);
+})
 
 export default app;
