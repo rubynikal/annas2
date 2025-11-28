3b8159eedbdf0a0e88b333f8408ad63827212547
rubynikal <nika.zaharova06@gmail.com>
2025-11-28 18:03:22 +0000

    october and november

--- database-migrations/V5__items_and_users_to_items.sql
+++ database-migrations/V5__items_and_users_to_items.sql
@@ -10,3 +10,23 @@ CREATE TABLE users_to_items (
 );
 
 INSERT INTO items (name) SELECT 'Item ' || (n)::text FROM generate_series(1, 1000) n;
+
+WITH random_items AS (
+	SELECT
+		u.id AS user_id,
+		i.id AS item_id,
+		ROW_NUMBER() OVER (PARTITION BY u.id ORDER BY RANDOM()) as rn
+	FROM
+		users u
+	CROSS JOIN
+		items i
+)
+
+INSERT INTO users_to_items (user_id, item_id)
+SELECT
+	user_id,
+	item_id
+FROM
+	random_items
+WHERE
+	rn<=5;
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import { Hono } from '@hono/hono';
 import { cors } from '@hono/hono/cors';
 import { logger } from '@hono/hono/logger';
 import postgres from "postgres";
+import { cache } from "@hono/hono/cache";
 
 const app = new Hono();
 const sql = postgres();
@@ -15,6 +16,17 @@ app.get("/", (c) => c.json({message: "Hello world!" }));
 app.get("/todos", async (c) => {
     const todos = await sql`SELECT * FROM todos`;
     return c.json(todos);
+})
+
+app.get("/cache-demo", cache({
+    cacheName: "demo-cache",
+    wait: true,
+}))
+
+//cache
+app.get("/cache-demo", async (c) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return c.json({message: "cachedemo"});
 })
 
 export default app;
