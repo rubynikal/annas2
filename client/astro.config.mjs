0dcd2889dc2fcc9a0db6f084da07d5283f0402f6
rubynikal <nika.zaharova06@gmail.com>
2026-02-17 18:23:13 +0000

    17.02.2026

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { deno } from'deno-adapter';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: deno(),
+});
