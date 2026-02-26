0f00c67a6e3f141cf9d1ccb99fb31505628c47e5
rubynikal <nika.zaharova06@gmail.com>
2026-02-26 20:49:50 +0000

    26.02.2026

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import deno from '@deno/astro-adapter';
 import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: deno(),
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
 });
